041f22cd10e715631e708a0d5e84c9c8b42e4ee4
alifaisal987 <alifaisal987@gmail.com>
2024-07-03 23:47:58 +0500

    alert assignment chap 1

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// QUESTION 1
+alert ("HELLO JAVA SCRIPT USER");
+
+// QUESTION 2
+alert ("Error! Please Enter a Valid Password");
+
+//QUESTION 3
+alert ("WELCOME TO JS LAND.... \n HAPPY CODING!");
+
+//QUESTION 4
+alert ("Welcome to Js Land....");
+
+//QUESTION 5
+alert('HAPPY CODING');
